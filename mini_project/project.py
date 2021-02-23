514a7e9b78c996881342288a08e3417a79a8f7c9
99003783 <78848745+99003783@users.noreply.github.com>
2021-02-23 18:19:38 +0530

    updated eod code

--- mini_project/project.py
+++ mini_project/project.py
@@ -1,8 +1,45 @@
 import re
-rewq = open("rb", 'C:\Users\99003783\Documents\GitHub\Python_module\mini_project\input.txt') 
-print(rewq.read())
-#der = re.match(r'\d (1-4)'"f_io")
+
+
+count = 0
+lines = 0
+
+rewq = open(r"C:\Users\99003783\Documents\GitHub\Python_module\mini_project\input.txt") 
+filename= rewq.read()
+qwe = input("enter the first keyword")
+l = list(qwe)
+
+pattern = re.findall(qwe,filename,re.M|re.I)
+
 #print(der)
+#print(len(der))this
 
 
-#
+
+for fde in filename:
+    
+    lines+=1
+    print(fde)
+if len(qwe) != None:
+    l.append((lines,qwe.rstrip('\n')))
+
+
+for answer in pattern:   
+    count+=1
+with open(qwe +".txt",'a') as file_answer:
+        file_answer.writelines(str(count)+' :')
+        file_answer.writelines(answer[0]+ '\n')
+
+
+
+
+#for answer in qwe:
+  #  count+=1
+#with open("qwe.txt",'a') as file_answer:
+ #       file_answer.writelines(str(count)+' :')
+  #      file_answer.writelines(answer[1]+ '\n')
+
+
+
+    
+
