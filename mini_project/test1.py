08857fc551671401c4c1930610b5ec10d51b9d1f
99003783 <78848745+99003783@users.noreply.github.com>
2021-02-23 18:20:47 +0530

    eod code

---
+++ mini_project/test1.py
@@ -0,0 +1,8 @@
+import re
+word = input("enter the word")
+f_r =open(r"C:\\Users\\99003783\\Documents\\GitHub\\Python_module\\mini_project\\input.txt") 
+f= f_r.read()
+count = re.findall(word, f, re.M | re.I)
+print(len(count))
+file2=open('a',"C:\Users\9003783\Documents\GitHub\Python_module\mini_project\output_file.txt")
+file2.write(len(count))
