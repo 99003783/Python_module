fd66d7de02fbbc92d5bf7dc51288bf9e06fcde87
99003783 <78848745+99003783@users.noreply.github.com>
2021-02-23 12:14:28 +0530

    mini project

--- mini_project/project.py
+++ mini_project/project.py
@@ -1,4 +1,8 @@
 import re
-f_io = open(r"C:\Users\99003783\Documents\GitHub\Python_module\input.txt") 
-der = re.match(r'\d (1-4)'f_io)
-print(der)
+rewq = open("C:\Users\99003783\Documents\GitHub\Python_module\input.txt", "r") 
+print(rewq.read())
+#der = re.match(r'\d (1-4)'"f_io")
+#print(der)
+
+
+#
