df504952272318622d36eee796f84e1266a183ab
99003783 <78848745+99003783@users.noreply.github.com>
2021-02-23 12:20:05 +0530

    updating project.py

--- mini_project/project.py
+++ mini_project/project.py
@@ -1,5 +1,5 @@
 import re
-rewq = open("C:\Users\99003783\Documents\GitHub\Python_module\input.txt", "r") 
+rewq = open("rb", 'C:\Users\99003783\Documents\GitHub\Python_module\mini_project\input.txt') 
 print(rewq.read())
 #der = re.match(r'\d (1-4)'"f_io")
 #print(der)
